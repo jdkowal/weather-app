6b6fc67088a24b7bbbf2cb0946b69e0ab3efa965
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-10-11 21:51:48 -0500

    got bootsrap to look good and fomrmatted css

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var cityInput = document.querySelector('#city-input');
+var cityBtn = document.querySelector('#search-btn');
+var cityNameEl = document.querySelector('#city-name');
+var cityArr = [];
+var apiKey = "46859f1820503d06f10d07676350cfbe"; 
+
